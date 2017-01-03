a7a32675eeedd4fc388ed7a51ad833799e9515d1
Cc <vaal.just@qq.com>
2017-01-03 16:15:06 +0800

    finished time param adding

--- src/main/java/zju/ccnt/mdsp/settings/Constant.java
+++ src/main/java/zju/ccnt/mdsp/settings/Constant.java
@@ -7,9 +7,16 @@ package zju.ccnt.mdsp.settings;
 public class Constant {
     public static final String FRONTEND_KEY = "";
     public static final String USER_INFO_SYSTEM_KEY = "";
+
+    // server
     public static final String HIS_URL = "http://localhost:8000/mdsp-his";
     public static final String USER_INFO_SYSTEM_URL = "http://localhost:8000/mdsp-his";
-    public static final String[] PRIVACY_FIELDS = new String[] { "id", "name", "age"
+
+    // local
+//    public static final String HIS_URL = "http://localhost:7070";
+//    public static final String USER_INFO_SYSTEM_URL = "http://localhost:7070";
+
+    public static final String[] PRIVACY_FIELDS = new String[]{"id", "name", "age"
             , "idcard", "password", "phone", "email", "userId", "gender", "demander"
             , "coroner", "checker", "assayId", "userId", "patient", "recordId"
             , "address", "doctor", "recipeId", "drugItemId"};
